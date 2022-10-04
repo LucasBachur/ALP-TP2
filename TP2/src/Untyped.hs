c44e9bed3a22f55831c5570e1c4f0adafedcc2cc
Lucas Bachur <lucasbachur11@gmail.com>
2022-10-04 18:06:24 -0300

    Agregados los primeros 4

--- TP2/src/Untyped.hs
+++ TP2/src/Untyped.hs
@@ -12,22 +12,33 @@ import           Common
 ----------------------------------------------
 
 conversion :: LamTerm -> Term
-conversion = undefined
+conversion lt = case lt of 
+                    LVar var -> Free (Global var)
+                    App lt1 lt2 -> (conversion lt1) :@: (conversion lt2) 
+                    Abs var lt1 -> Lam (replace var 0 (conversion lt1))
+                    where replace v n a@(Bound m) = a
+                          replace v n a@(Free (Global l)) | v == l = Bound n
+                                                          | otherwise = a
+                          replace v n (t1 :@: t2) = (replace v n t1) :@: (replace v n t2)
+                          replace v n (Lam t) = Lam (replace v (n+1) t)
+                 
 
 -------------------------------
 -- Sección 3
 -------------------------------
 
 vapp :: Value -> Value -> Value
-vapp = undefined
+vapp (Vlam f) v = f v
+vapp (VNeutral n) v = VNeutral (NApp n v)
 
 eval :: NameEnv Value -> Term -> Value
 eval e t = eval' t (e, [])
 
 eval' :: Term -> (NameEnv Value, [Value]) -> Value
-eval' (Bound ii) (_, lEnv) = lEnv !! ii
-eval' _          _         = undefined
-
+eval' (Bound ii) (_, lEnv)  = lEnv !! ii
+eval' (Free v)   (gEnv, _)  = gEnv !! v
+eval' (t1 :@: t2) e         = vapp (eval' t1 e) (eval' t2 e)
+eval' (Lam t) (gEnv, lEnv)  = VLam (\val -> eval' t (gEnv, val:lEnv))
 
 --------------------------------
 -- Sección 4 - Mostrando Valores
