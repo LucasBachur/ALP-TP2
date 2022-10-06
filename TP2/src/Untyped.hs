07147c7b133a102c562f14cc194941dbff46380b
Lucas Bachur <lucasbachur11@gmail.com>
2022-10-06 19:52:54 -0300

    No anda el 6 :)

--- TP2/src/Untyped.hs
+++ TP2/src/Untyped.hs
@@ -7,7 +7,7 @@ import           Data.Maybe
 import           Common
 
 ----------------------------------------------
--- Seccón 2  
+-- Sección 2  
 -- Ejercicio 2: Conversión a términos localmente sin nombres
 ----------------------------------------------
 
@@ -28,7 +28,7 @@ conversion lt = case lt of
 -------------------------------
 
 vapp :: Value -> Value -> Value
-vapp (Vlam f) v = f v
+vapp (VLam f) v = f v
 vapp (VNeutral n) v = VNeutral (NApp n v)
 
 eval :: NameEnv Value -> Term -> Value
@@ -36,7 +36,9 @@ eval e t = eval' t (e, [])
 
 eval' :: Term -> (NameEnv Value, [Value]) -> Value
 eval' (Bound ii) (_, lEnv)  = lEnv !! ii
-eval' (Free v)   (gEnv, _)  = gEnv !! v
+eval' (Free var) (gEnv, _)  = search var gEnv 
+                                    where search v [] = VNeutral (NFree v)
+                                          search v ((name,val):xs) = if v == name then val else search v xs
 eval' (t1 :@: t2) e         = vapp (eval' t1 e) (eval' t2 e)
 eval' (Lam t) (gEnv, lEnv)  = VLam (\val -> eval' t (gEnv, val:lEnv))
 
@@ -45,7 +47,18 @@ eval' (Lam t) (gEnv, lEnv)  = VLam (\val -> eval' t (gEnv, val:lEnv))
 --------------------------------
 
 quote :: Value -> Term
-quote = undefined
+quote value = quote' value 0
+
+quote' :: Value -> Int -> Term
+quote' (VLam fun) n                       = let val = (fun (VNeutral (NFree (Quote n))))
+                                                t = quote' val (n+1)
+                                                in Lam t
+quote' (VNeutral (NFree (Global var))) _  = Free (Global var)
+quote' (VNeutral (NFree (Quote m))) n     = Bound (n - m - 1)
+quote' (VNeutral (NApp neutral val)) n    = let t1 = (quote' (VNeutral neutral) n)
+                                                t2 = (quote' val n)
+                                            in t1 :@: t2
+
 
 
 
